2c494050e680930506e48386cad54d654f8ddede
Vaku1a <110764648+Vaku1a@users.noreply.github.com>
2023-10-01 21:40:24 +0300

    10 - done

--- js/task-09.js
+++ js/task-09.js
@@ -8,8 +8,10 @@ const wrapper = document.querySelector('.widget');
 // console.log(wrapper)
 const btnColorEl = document.querySelector('.change-color');
 // console.log(btnColorEl);
-btnColorEl.addEventListener('click', changeColor)
+const spanEl = document.querySelector('.color');
+btnColorEl.addEventListener('click', changeColor);
 
 function changeColor() {
-  wrapper.style.backgroundColor = getRandomHexColor()
+  wrapper.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,42 @@
+
+const wrapper = document.querySelector('#boxes');
+const inputEl = document.querySelector('[type="number"]');
+const createBtnEl = document.querySelector('[data-create]');
+const destroyBtnEl = document.querySelector('[data-destroy]');
+
+createBtnEl.addEventListener('click', onClickCreate);
+destroyBtnEl.addEventListener('click', onClickDestroy);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function onClickCreate() {
+  let divNumber = Number(inputEl.value);
+  for (let i = 1; i <= divNumber; i++) {
+    const div = document.createElement("div");
+    div.id = i; 
+    div.style.width = `${i * 10}px`;
+    div.style.height = `${i * 10}px`;
+    div.style.background = getRandomHexColor();
+
+    wrapper.appendChild(div);
+    
+  }
+}
+
+function onClickDestroy() {
+  while (wrapper.firstChild) {
+    wrapper.removeChild(wrapper.firstChild);
+  }
+}
+
+
+
+
+
+
+
+
